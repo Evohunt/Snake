68640d75bd0f7d375a995bae1d28159c2cf51203
Evohunt <evohuntmaster@gmail.com>
2018-07-26 17:31:05 +0300

    Created Application class

---
+++ src/Snake/Application.java
@@ -0,0 +1,18 @@
+package Snake;
+
+public class Application {
+
+    public static void main(String... Args) {
+
+        Application application = new Application();
+        application.launch();
+
+    }
+
+    private void launch() {
+
+        
+
+    }
+
+}
